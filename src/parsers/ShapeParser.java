8be605e35ce38a1924bbcd24a91c78947795274c
Alexandre Moine <alexandre@moine.me>
2018-04-03 14:31:47 +0000

    Update ShapeParser.java

--- src/parsers/ShapeParser.java
+++ src/parsers/ShapeParser.java
@@ -12,7 +12,7 @@ import src.exceptions.*;
 import src.ast.*;
 import src.utils.Tuple;
 
-public class ShapeParser extends Parser<Tuple<Shape,Color>> {
+public class ShapeParser extends Parser<Instr<Shape>> {
 
   private ParserExpr expr;
 
@@ -20,34 +20,34 @@ public class ShapeParser extends Parser<Tuple<Shape,Color>> {
     expr = new ParserExpr();
   }
 
-  public Tuple<Shape,Color> parse (AST<?> a) 
+  public Instr<Shape> parse (AST<?> ast) 
       throws Exception, UnexpectedSymbolException {
     if (r.is(Sym.CIRCLE)) {
       r.eat(Sym.CIRCLE);
       r.eat(Sym.LPAR);
-      int x = expr.parse(a).eval();
+      Expr x = expr.parse(ast);
       r.eat(Sym.COMA);
-      int y = expr.parse(a).eval();
+      Expr y = expr.parse(ast);
       r.eat(Sym.COMA);
-      int rad = expr.parse(a).eval();
+      Expr rad = expr.parse(ast);
       r.eat(Sym.COMA);
       Color c = r.pop(Color.class).getObject();
       r.eat(Sym.RPAR);
-      return new Tuple<Shape,Color>(new Ellipse2D.Double(x-rad, y-rad, 2*rad, 2*rad),c);
+      return new InstrShape(Ellipse2D.Double::new,new ExprOp((a,b)->a-b,"-",x,rad), new ExprOp((a,b) -> a-b,"-",y,rad),new ExprOp((a,b)->a*b,"*", new ExprLeaf(2),rad),new ExprOp((a,b)->a*b,"*",new ExprLeaf(2),rad),c);
     }
 
     r.eat(Sym.RECT);
     r.eat(Sym.LPAR);
-    int x = expr.parse(a).eval();
+    Expr x = expr.parse(ast);
     r.eat(Sym.COMA);
-    int y = expr.parse(a).eval();
+    Expr y = expr.parse(ast);
     r.eat(Sym.COMA);
-    int width = expr.parse(a).eval();
+    Expr width = expr.parse(ast);
     r.eat(Sym.COMA);
-    int height = expr.parse(a).eval();
+    Expr height = expr.parse(ast);
     r.eat(Sym.COMA);
     Color c = r.pop(Color.class).getObject();
     r.eat(Sym.RPAR);
-    return new Tuple<Shape,Color>(new Rectangle2D.Double(x, y, width, height),c);
+    return new InstrShape(Rectangle2D.Double::new,x, y, width, height,c);
   }
 }
