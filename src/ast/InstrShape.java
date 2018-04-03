1c63e9bad7d4f9f64dfa2cc4e3384e3977ef418e
Alexandre Moine <alexandre@moine.me>
2018-04-03 14:29:47 +0000

    Update InstrShape.java

--- src/ast/InstrShape.java
+++ src/ast/InstrShape.java
@@ -4,17 +4,27 @@ import java.awt.Shape;
 import java.awt.Graphics2D;
 import java.awt.Color;
 
-public class InstrShape implements Instr<Void> {
-  public final Shape shape;
+public class InstrShape implements Instr<Shape> {
+  public final ShapeBuilder shapeb;
+  public final Expr a,b,c,d;
   public final Color color;
 
-  public InstrShape(Shape shape, Color color){
-    this.shape = shape;
+  public InstrShape(ShapeBuilder shape, Expr a, Expr b, Expr c, Expr d, Color color){
+    this.shapeb=shape;
+    this.a=a;
+    this.b=b;
+    this.c=c;
+    this.d=d;
     this.color = color;
   }
 
-  public Void eval(Graphics2D g){
+  public Shape eval(Graphics2D g){
     g.setPaint(color);
-    return null;
+    return shapeb.eval(a.eval(),b.eval(),c.eval(),d.eval());
+  }
+
+  @FunctionalInterface
+  public interface ShapeBuilder{
+    public Shape eval(double a, double b, double c, double d);
   }
 }
